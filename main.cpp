e6df1149e5f5b71466d4b34a0657ad0117c94429
Mayand Singh <112964733+Rathode117@users.noreply.github.com>
2023-09-03 11:10:08 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include<bits/stdc++.h>
+#define ll long long int
+#define rep(i,x,n) for(int i=x;i<n;i++)
+#define rrep(i,n) for(int i=n-1;i>=0;i--)
+#define out(x) cout<<x<<endl;
+#define F first
+#define S second
+#define vi vector<int>
+#define vl vector<long long int>
+#define put(x,arr) arr.push_back(x);
+using namespace std;
+int main(){
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vi arr;
+        rep(i,0,n){
+            int x;
+            cin>>x;
+            put(x,arr);
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+    return 0;
+
+}
